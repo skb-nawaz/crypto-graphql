1ec7eca3ea63244d9e602e9a9c932d18a2e2a018
skb-nawaz <125886500+skb-nawaz@users.noreply.github.com>
2023-10-09 19:24:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ const RootQuery = new GraphQLObjectType({
 							params: {
 								vs_currency: 'usd',
 								order: 'market_cap_desc',
-								per_page: 20,
+								per_page: 100,
 								page: 1,
 								sparkline: false,
 								
